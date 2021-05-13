c3554ff68e80a5a727629da201db32ecff8aa85b
RafaelBarrero <ramag874@gmail.com>
2021-05-13 19:36:45 +0200

    main.py: main script to decide with one of the speeches use

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from src.google.google_speech import google_voice_recognition
+from src.wit.wit_speech import wit_voice_recognition
+
+if __name__ == '__main__':
+    # google_voice_recognition()
+    wit_voice_recognition()
