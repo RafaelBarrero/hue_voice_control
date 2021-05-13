1ffe15da75f7254999e006d6b8bd4a7ea30a1d50
RafaelBarrero <ramag874@gmail.com>
2021-05-13 22:43:01 +0200

    speech.py: implement thread class to get feedback and more realistic assistant
    * Refactor code to get more clarity

--- src/speech/speech.py
+++ src/speech/speech.py
@@ -5,62 +5,81 @@ from dotenv import load_dotenv
 
 import speech_recognition as sr
 
+from src.speech.voice_thread import Voice
+
 load_dotenv()
 
 WIT_AI_KEY = os.getenv('WIT_AI_KEY')
 
 r = sr.Recognizer()
 source = sr.Microphone()
+
 wit = False
 salir = False
+
+voice = Voice()
 
 
 def callback(recognizer, audio):
     global salir
-    # received audio data, now we'll recognize it using Google Speech Recognition
+
     try:
         if wit:
             recognized: str = recognizer.recognize_wit(audio, key=WIT_AI_KEY)
         else:
             recognized: str = recognizer.recognize_google(audio, language="es-ES")
-        print(f"He reconocido {recognized} en callback")
-        if recognized and "jarvis" in recognized.lower() or "darvish" in recognized.lower():
+        print(f"He reconocido {recognized.strip()} en callback")
+
+        if recognized.strip() != "" and "jarvis" in recognized.lower() or "darvish" in recognized.lower():
+            voice.say("¿Me llamabas?")
             jarvis()
-        elif recognized and "salir" in recognized.lower():
+        elif recognized.strip() != "" and "salir" in recognized.lower():
             print("salir")
+            voice.say("Cerrando script")
             salir = True
+
     except (sr.UnknownValueError, sr.RequestError) as e:
         print(f"No se ha podido reconocer la voz; {e}")
 
 
 def voice_recognition(recognition_str: str = None):
     global wit
+
     if recognition_str == "wit":
         wit = True
     with source as mic:
         r.adjust_for_ambient_noise(mic)
+
     stop = r.listen_in_background(source, callback)
     print("Di algo")
+
     while not salir:
         time.sleep(0.1)
+
     stop()
+    voice.terminate()
 
 
 def jarvis():
     global wit
-    print("Jarvis")
+    print("jarvis")
+
     audio = r.listen(source)
+
     try:
         if wit:
             recognized: str = r.recognize_wit(audio, key=WIT_AI_KEY)
         else:
             recognized: str = r.recognize_google(audio, language="es-ES")
-        print(f"He reconocido {recognized} en jarvis")
-        if recognized and "morado" in recognized.lower():
-            print("morado")
-        elif recognized and "apagar" in recognized.lower():
-            print("apagar")
+        print(f"He reconocido {recognized.strip()} en jarvis")
+
+        if recognized.strip() != "" and "morado" in recognized.lower():
+            voice.say("Cambiando las luces a morado")
+        elif recognized.strip() != "" and "salir" in recognized.lower():
+            voice.say("Adiós, Rafa")
             return
+
+        # Calls the same function to start a loop if not "apagar"
         jarvis()
     except (sr.UnknownValueError, sr.RequestError) as e:
         print(f"No se ha podido reconocer la voz; {e}")
